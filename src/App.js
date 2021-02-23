122aed37ab3f65ab05fa390d38833d68188c6c27
Sean Leonard <seanleonardsap@gmail.com>
2021-02-23 17:12:58 +0000

    class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-function App({ title }) {
-  return (
-    <div className="App">
-      <h1>{title}</h1>
-    </div>
-  );
+import "./styles/style.css";
+import React from "react";
+
+// Card
+class Card extends React.Component {
+  render() {
+    return <div className="github-profile">One github profile</div>;
+  }
 }
+// List of Cards
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        {this.props.title}
+        <Card />
+      </div>
+    );
+  }
+}
+
+//function App({ title }) {
+//  return (
+//    <div className="App">
+//      <h1>{title}</h1>
+//    </div>
+//  );
+// }
 
 export default App;
