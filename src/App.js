4019459a113d3cc6362133b1792935af56fb009f
Sean Leonard <seanleonardsap@gmail.com>
2021-02-23 17:40:02 +0000

    hard coded data and card list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import "./styles/style.css";
 import React from "react";
 
+// test data
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook",
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu",
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook",
+  },
+];
+
+// List of Cards
+const CardList = (props) => (
+  <div>
+    {testData.map((profile) => (
+      <Card {...profile} />
+    ))}
+  </div>
+);
+
 // Card
 class Card extends React.Component {
   render() {
+    const profile = this.props;
     return (
       <div className="github-profile">
-        <img src="https://placehold.it/75" />
+        <img src={profile.avatar_url} />
         <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
   }
 }
-// List of Cards
 
 class App extends React.Component {
   render() {
     return (
       <div className="Header">
         {this.props.title}
-        <Card />
+        <CardList />
       </div>
     );
   }
