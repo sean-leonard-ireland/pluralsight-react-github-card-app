a610dfc67b0e3866c3f6912269cb92cd77315989
Sean Leonard <seanleonardsap@gmail.com>
2021-02-23 18:05:31 +0000

    form + handle input + state

--- src/App.js
+++ src/App.js
@@ -22,10 +22,22 @@ const testData = [
 
 //Form
 class Form extends React.Component {
+  state = { userName: "" };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.userName);
+  };
+
   render() {
     return (
-      <form action="">
-        <input type="text" placeholder="GitHub username" />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder="GitHub username"
+          required
+        />
         <button>Add card</button>
       </form>
     );
